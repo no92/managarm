a161ab2449726508d0576a3f424c4456ec5c81a7
no92 <no92.mail@gmail.com>
2023-01-24 11:48:08 +0100

    netserver: handle RTM_GETROUTE netlink requests

---
+++ servers/netserver/src/netlink/utils.cpp
@@ -0,0 +1,17 @@
+#include "netlink.hpp"
+#include "src/netlink/packets.hpp"
+
+namespace nl {
+
+void NetlinkSocket::sendDone(struct nlmsghdr *hdr) {
+	assert(hdr->nlmsg_len >= sizeof(*hdr));
+
+	NetlinkBuilder b;
+
+	b.header(NLMSG_DONE, 0, hdr->nlmsg_seq, 0);
+	b.message<uint32_t>(0);
+
+	_recvQueue.push_back(b.packet());
+}
+
+} // namespace nl
