c3fa54c276d02e2153cf8afba12dfdfb19bae156
Dennis Bonke <admin@dennisbonke.com>
2025-01-10 00:49:22 +0100

    posix: Replace wrong assert with a proper if statement

--- posix/subsystem/src/fs.cpp
+++ posix/subsystem/src/fs.cpp
@@ -8,8 +8,9 @@
 // --------------------------------------------------------
 
 async::result<frg::expected<Error>> FsLink::obstruct() {
-	assert(getOwner());
-	assert(!getOwner()->hasTraverseLinks() && "Node has traverseLinks but no obstruct?");
+	// assert(getOwner());
+	if(getOwner() != nullptr)
+		assert(!getOwner()->hasTraverseLinks() && "Node has traverseLinks but no obstruct?");
 	co_return Error::illegalOperationTarget;
 }
 
